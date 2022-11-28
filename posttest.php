3d18a6fda5d415bdca7d87fd3f6f710260f2fbac
Joshua Varghese <joshvarg.sec@gmail.com>
2022-11-28 17:23:36 -0500

    Add null query check for patient query

--- posttest.php
+++ posttest.php
@@ -3,8 +3,12 @@
 <?php $conn = new mysqli('localhost', 'root', 'mysql', 'OHC');
 	$sql = "select pName FROM PATIENT where pName='".$_POST["user"]."'";
 	$result = mysqli_query($conn, $sql);
-	$row = mysqli_fetch_array($result);
+  $row = mysqli_fetch_array($result);
+  if ($row == null) {
+    echo "<p>Error, no patient found!</p><br><a href='register.php'>Register Patient</a>";
+  } else {
+    echo "User: ".$row[0];
+  }
 ?>
-User: <?php echo $row[0]; ?>
 </body>
 </html>
